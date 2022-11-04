754125a7b69fc1e7ace3c1ac822f182248ec7670
JoseAVallejo12 <josealfredovallejo25@gmail.com>
2022-11-04 18:55:11 -0500

    add tailwind css to the project

--- src/app/core/renderer/index.js
+++ src/app/core/renderer/index.js
@@ -3,7 +3,8 @@ import { WebGLRenderer } from 'three';
 function createRenderer(containerId) {
   const container = document.getElementById(containerId);
   const renderer = new WebGLRenderer();
-  renderer.setSize(window.innerWidth / 1.2, window.innerHeight);
+  renderer.setSize(window.innerWidth / 1.5, window.innerHeight);
+  // renderer.setSize(container?.clientWidth, window.innerHeight);
   container.appendChild(renderer.domElement);
   return renderer;
 }
